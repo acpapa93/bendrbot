cf7d2c9714567b83ffea5113d36c5c7bf4345926
acpapa93 <acpapa93#gmail.com>
2016-12-08 10:59:32 -0600

    Add existing files

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 //google sheet vars and auths
 var Sheet_ID = process.env.SHEET_ID;
 var Auth_key = process.env.Auth_Key;
-var name1, rent1, name2, rent2, name3, rent3, name4, rent4, name5, rent5, name6, rent6, name7, rent7, rentStatement = "";
+var name1, rent1, name2, rent2, name3, rent3, name4, rent4, name5, rent5, name6, rent6, name7, rent7, rentStatement, owes, per = "";
 
 //request parameters
 var gUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + Sheet_ID + "/values/I25%3AJ32?majorDimension=ROWS&key=" + Auth_key;
@@ -31,7 +31,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "GET RENTY" + parsed;
+  botResponse = "GET RENTY" + rentStatement;
 
   options = {
     hostname: 'api.groupme.com',
@@ -68,6 +68,8 @@ exports.respond = respond;
 
 //googleSheetCall.js below///////////
 var parsed="";
+owes=" owes ";
+per = ".";
 function googlePull() {
     HTTPS.get(gUrl, function(res) {
         var body ="";
@@ -84,32 +86,31 @@ function googlePull() {
           parsed= JSON.parse(body);
           console.log(parsed);
 
-          /*
-            name1: data.values[1][0],
-            rent1: data.values[1][1],
-            name2: data.values[2][0],
-            rent2: data.values[2][1],
-            name3: data.values[3][0],
-            rent3: data.values[3][1],
-            name4: data.values[4][0],
-            rent4: data.values[4][1],
-            name5: data.values[5][0],
-            rent5: data.values[5][1],
-            name6: data.values[6][0],
-            rent6: data.values[6][1],
-            name7: data.values[7][0],
-            rent7: data.values[7][1]},
+          name1 = parsed.values[1][0];
+          rent1 = parsed.values[1][1];
+          name2 = parsed.values[2][0];
+          rent2 = parsed.values[2][1];
+          name3 = parsed.values[3][0];
+          rent3 = parsed.values[3][1];
+          name4 = parsed.values[4][0];
+          rent4 = parsed.values[4][1];
+          name5 = parsed.values[5][0];
+          rent5 = parsed.values[5][1];
+          name6 = parsed.values[6][0];
+          rent6 = parsed.values[6][1];
+          name7 = parsed.values[7][0];
+          rent7 = parsed.values[7][1];
 
-            var oneStatement = name1 + " owes " + rent1 + ".";
-            var twoStatement = " " + name2 + " owes " + rent2 + ".";
-            var threeStatement = " " + name3 + " owes " + rent3 + ".";
-            var fourStatement = " " + name4 + " owes " + rent4 + ".";
-            var fiveStatement = " " + name5 + " owes " + rent5 + ".";
-            var sixStatement = " " + name6 + " owes " + rent6 + ".";
-            var sevStatement = " " + name7 + " owes " + rent7 + ".";
+            var oneStatement = name1 + owes + rent1 + per;
+            var twoStatement = " " + name2 + owes + rent2 + per;
+            var threeStatement = " " + name3 + owes + rent3 + per;
+            var fourStatement = " " + name4 + owes + rent4 + per;
+            var fiveStatement = " " + name5 + owes + rent5 + per;
+            var sixStatement = " " + name6 + owes + rent6 + per;
+            var sevStatement = " " + name7 + owes+ rent7 + per;
             rentStatement = oneStatement + twoStatement + threeStatement + fourStatement + fiveStatement + sixStatement + sevStatement;
-*/
-            postMessage(parsed);
+
+            postMessage(rentStatement);
           });
         });
       }
